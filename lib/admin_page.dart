a1b6c5800a15a669d48e350c39153d483e607ae9
Karthik <karthiln121@gmail.com>
2022-03-27 16:40:43 +0530

    mar 27

--- lib/admin_page.dart
+++ lib/admin_page.dart
@@ -2,53 +2,59 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:untitled1/student.dart';
 
-
-
 class Admin extends StatelessWidget {
   Admin({Key? key}) : super(key: key);
-  final String id='admin_page';
-  final messageTextController=TextEditingController();
+  final String id = 'admin_page';
+  final messageTextController = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    final _firestore=FirebaseFirestore.instance;
+    final _firestore = FirebaseFirestore.instance;
 
-     String messageText="50";
+    String messageText = "50";
     return Scaffold(
       appBar: AppBar(
         title: Text("admin consol"),
+        backgroundColor: Colors.deepPurple[900],
       ),
       body: Column(
-        children:  [
+        children: [
           Padding(
-             padding: EdgeInsets.all(50),
+            padding: EdgeInsets.all(50),
             child: TextField(
-                  controller: messageTextController,
-                  decoration: InputDecoration(
-                    hintText: "enter time only",
-                  ),
-              onChanged: (text){
-                messageText=text;
-
+              controller: messageTextController,
+              decoration: InputDecoration(
+                focusedBorder: OutlineInputBorder(
+                    borderSide:
+                        BorderSide(color: Colors.deepPurpleAccent, width: 2)),
+                enabledBorder: OutlineInputBorder(
+                  borderSide:
+                      BorderSide(color: Colors.deepPurpleAccent, width: 2.0),
+                ),
+                hintText: "Enter Time only",
+              ),
+              onChanged: (text) {
+                messageText = text;
               },
             ),
           ),
           FloatingActionButton(
+              backgroundColor: Colors.pinkAccent,
               onPressed: () {
                 messageTextController.clear();
-                final mess=Messages(text: messageText,createdAt: FieldValue.serverTimestamp());
+                final mess = Messages(
+                    text: messageText, createdAt: FieldValue.serverTimestamp());
 
                 createUser(mess);
               },
-              child:Text("post")),
-
+              child: Text("post")),
         ],
       ),
     );
-    
   }
-  Future createUser(Messages user)async{
-    final docUser=FirebaseFirestore.instance.collection('user').doc();
-  final json= user.toJson();
-  await docUser.set(json);
+
+  Future createUser(Messages user) async {
+    final docUser = FirebaseFirestore.instance.collection('user').doc();
+    final json = user.toJson();
+    await docUser.set(json);
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:untitled1/admin_page.dart';
@@ -7,7 +6,6 @@ import 'package:untitled1/student.dart';
 import 'admin_login.dart';
 
 void main() async {
-
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
 
@@ -15,12 +13,10 @@ void main() async {
     MaterialApp(
       initialRoute: FirstScreen().id,
       routes: {
-        FirstScreen().id:(context)=>FirstScreen(),
-        Admin().id:(context)=>Admin(),
-        Login().id:(context)=>Login(),
+        FirstScreen().id: (context) => FirstScreen(),
+        Admin().id: (context) => Admin(),
+        Login().id: (context) => Login(),
       },
     ),
   );
 }
-
-
